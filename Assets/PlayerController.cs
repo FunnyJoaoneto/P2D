aabba6949df2d3b7250478359fe371996eb5c6ec
João Neto <jneto04@ua.pt>
2025-10-18 19:02:30 +0100

    testing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,34 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 7f;
-    public PlayerInput playerInput;
-
     private Rigidbody2D rb;
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    [Header("Jump Settings")]
+    public float jumpForce = 7f;
+    public int maxJumps = 2;
+    private int jumpsRemaining;
+    
+    [Header("Ground Check")]
+    public LayerMask groundLayer;
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+
+    [Header("Gravity Settings")]
+    public float baseGravity = 2f;
+    public float fallSpeedMultiplier = 2f;
+    public float maxFallSpeed = 18f;
+    private bool isGliding = false;
+
+    [Header("Abilities")] // This can be changed later
+    public bool canGlide = false;
+    public float glideGravityScale = 0.3f;
+    public float glideUpBoost = 2f;
+    private bool jumpHeld = false;
+
+    public PlayerInput playerInput;
     private Vector2 moveInput;
-    private bool isGrounded = false;
 
     void Awake()
     {
@@ -21,26 +42,39 @@ public class PlayerController : MonoBehaviour
     void OnEnable()
     {
         var actions = playerInput.actions;
-        actions["Move"].performed += ctx => {
-            moveInput = ctx.ReadValue<Vector2>();
-            //Debug.Log($"{gameObject.name} Move: {moveInput}");
-        };
-        actions["Move"].canceled += ctx => {
-            moveInput = Vector2.zero;
-            //Debug.Log($"{gameObject.name} Move canceled");
-        };
-        actions["Jump"].performed += ctx => {
-            //Debug.Log($"{gameObject.name} Jump pressed");
-            Jump();
-        };
+        actions["Move"].performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        actions["Move"].canceled += ctx => moveInput = Vector2.zero;
+        actions["Jump"].performed += ctx => Jump(true);
+        actions["Jump"].canceled += ctx => Jump(false);
     }
 
     void OnDisable()
     {
-        var actions = playerInput.actions;
-        actions["Move"].performed -= ctx => moveInput = ctx.ReadValue<Vector2>();
-        actions["Move"].canceled -= ctx => moveInput = Vector2.zero;
-        actions["Jump"].performed -= ctx => Jump();
+        playerInput.actions.Disable();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        Gravity();
+    }
+
+    private void Gravity()
+    {
+        if(rb.linearVelocity.y < 0)
+        {
+            if (isGliding){
+                rb.gravityScale = glideGravityScale;
+            }
+            else{
+                rb.gravityScale = baseGravity * fallSpeedMultiplier;
+            }
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
 
     void FixedUpdate()
@@ -48,18 +82,37 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
     }
 
-    void Jump()
+    void Jump(bool jumping)
     {
-        if (isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
+        Debug.Log("Jumps Remaining: " + jumpsRemaining);
+        jumpHeld = jumping;
+
+        if (jumpsRemaining > 0){
+            if (jumping)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                jumpsRemaining--;
+            }
+            else
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                jumpsRemaining--;
+            }
         }
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    private void GroundCheck()
     {
-        if (col.contacts[0].normal.y > 0.5f)
-            isGrounded = true;
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0f, groundLayer))
+        {
+            isGliding = false;
+            jumpsRemaining = maxJumps;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
